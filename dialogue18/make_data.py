01324b98feb4e673f50cd0f0cf635a64e81cbdc3
Anna Luiza Caraciolo <alcaf@cin.ufpe.br>
2024-01-30 19:30:33 -0300

    Dataset ajustado para treinamento com keras

--- dialogue18/make_data.py
+++ dialogue18/make_data.py
@@ -1,14 +1,21 @@
-
 import json
 
-
 if __name__ == '__main__':
-    with open("dialogues_018.json", 'r') as file:
+    with open("dialogue18/dialogues_018.json", 'r') as file:
         data_018 = json.load(file)
 
     for i in range(len(data_018)):
-        current_speaker = data_018[i]
-        turns_amount = len(data_018[i]['turns'])
-        for j in range(turns_amount):
-            utterance = current_speaker['turns'][j]['utterance']
-            print(utterance)
+        frames_amount = len(data_018[i]['turns'])
+        for j in range(frames_amount):
+            current_frame = data_018[i]['turns'][j]
+            speaker = current_frame['speaker']
+            if speaker == 'USER':
+                intent = current_frame['frames'][0]['state']['active_intent']
+                utterance = current_frame['utterance']
+                
+                file_path = "c:\\Users\\Anna Luiza\\Documents\pln_chatbot\\dialogue18\\" + intent + "\\" + str(i) + "_" + str(j) + ".txt"
+                
+                f = open(file_path, "w+")
+                f.write(utterance)
+                f.close()
+        
