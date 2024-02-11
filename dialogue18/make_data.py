5b421f66936956e64d52456240ebcbbf0d0d26c4
Anna Luiza <alcaf@cin.ufpe.br>
2024-02-11 17:16:49 -0300

    Adaptando dados para o rasa e treinando novo modelo no rasa

--- dialogue18/make_data.py
+++ dialogue18/make_data.py
@@ -1,8 +1,22 @@
 import json
+import yaml
+import ruamel.yaml
+import sys
+
+def literal(*args):
+    # convert args to a multiline string that looks like block sequence
+    # convert to string in case non-strings were passed in
+    return ruamel.yaml.scalarstring.LiteralScalarString('- ' + '\n- '.join([str(x) for x in args]) + '\n')   
 
 if __name__ == '__main__':
     with open("dialogue18/dialogues_018.json", 'r') as file:
         data_018 = json.load(file)
+
+    NONE_list = []
+    SearchOnewayFlight_list = []
+    ReserveOnewayFlight_list = []
+    SearchRoundtripFlights_list = []
+    ReserveRoundtripFlights_list = []
 
     for i in range(len(data_018)):
         frames_amount = len(data_018[i]['turns'])
@@ -18,4 +32,34 @@ if __name__ == '__main__':
                 f = open(file_path, "w+")
                 f.write(utterance)
                 f.close()
+
+                # adaptando conjunto de dados para o RASA
+                if intent == 'SearchOnewayFlight':
+                    SearchOnewayFlight_list.append(utterance)
+                elif intent == 'ReserveOnewayFlight':
+                    ReserveOnewayFlight_list.append(utterance)
+                elif intent == 'SearchRoundtripFlights':
+                    SearchRoundtripFlights_list.append(utterance)
+                elif intent == 'ReserveRoundtripFlights':
+                    ReserveRoundtripFlights_list.append(utterance)
+                elif intent == 'NONE':
+                    NONE_list.append(utterance)
+    
+    
+    data = {'version': '3.1',
+    'nlu': [{'intent': 'SearchOnewayFlight', 'examples': literal(*SearchOnewayFlight_list)},
+    {'intent': 'ReserveOnewayFlight', 'examples': literal(*ReserveOnewayFlight_list)},
+    {'intent': 'SearchRoundtripFlights', 'examples': literal(*SearchRoundtripFlights_list)},
+    {'intent': 'ReserveRoundtripFlights', 'examples': literal(*ReserveRoundtripFlights_list)},
+    {'intent': 'NONE', 'examples': literal(*NONE_list)}]}
+        
+    yaml = ruamel.yaml.YAML()
+    #yaml.dump(data, sys.stdout)
+
+    # File path
+    file_path = 'output.yml'
+
+    # Open the file in write mode and use yaml.dump
+    with open(file_path, 'w') as file:
+        yaml.dump(data, file)
         
